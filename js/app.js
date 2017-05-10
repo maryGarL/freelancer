e443794bb123b4eb59e3548b54eb468e576bd068
maryGarL <maryel_565@hotmail.com>
2017-05-10 14:41:16 -0500

    scroll

--- js/app.js
+++ js/app.js
@@ -1,3 +1,22 @@
-/*
- * Archivo principal de JS
- */
+var portafolio=document.getElementById("portafolio");
+var about=document.getElementById("about");
+var contact=document.getElementById("contact");
+window.onscroll=function () {
+"use strict";
+console.log(document.body.scrollTop)
+if (document.body.scrollTop <= 559) {
+   portafolio.classList.remove("color");
+}
+else if (document.body.scrollTop >559 ||document.body.scrollTop < 1399) {
+
+         about.classList.remove("color");
+         portafolio.classList.add("color");
+}
+else if (document.body.scrollTop >= 1399 ||document.body.scrollTop < 1959){
+  portafolio.classList.remove("color");
+  about.classList.add("color");
+  contact.classList.remove("color");
+}else if (document.body.scrollTop >= 1959  ){
+about.classList.remove("color");
+contact.classList.add("color");
+}};
